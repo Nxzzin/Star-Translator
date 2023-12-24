a2c43dd273f77874444b8296421af33729827272
Name <pvpbaguio@gmail.com>
2023-12-24 16:54:58 -0300

    Update Instalar.bat

--- Versions/ENGLISH/Instalar.bat
+++ Versions/ENGLISH/Instalar.bat
@@ -1,3 +1,14 @@
 @echo off
-powershell.exe -ExecutionPolicy Bypass -File .\setup.ps1
-del %0
+setlocal
+
+:: Caminho do script PowerShell
+set "scriptPath=%~dp0setup.ps1"
+
+:: Executa o script PowerShell
+powershell.exe -ExecutionPolicy Bypass -File "%scriptPath%"
+
+:: Aguarda por 2 segundos
+timeout /t 2 /nobreak > nul
+
+:: Autoexclui o arquivo de lote
+del "%~f0"
