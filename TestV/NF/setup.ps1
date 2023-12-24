e1dbf90f45159915574b4faf3459141e8313bbbe
Name <pvpbaguio@gmail.com>
2023-12-24 11:32:08 -0300

    added TestV

---
+++ TestV/NF/setup.ps1
@@ -0,0 +1,28 @@
+# Caminho do arquivo de configuração
+$arquivoConfig = ".\USER.cfg"
+
+# Verifica se o arquivo USER.cfg existe
+if (Test-Path $arquivoConfig) {
+    # Lê o conteúdo do arquivo
+    $conteudo = Get-Content $arquivoConfig
+
+    # Verifica se as strings estão presentes no conteúdo
+    if ($conteudo -contains 'g_language = portuguese_(brazil)' -and $conteudo -contains 'g_languageAudio = english') {
+        # Se as strings estiverem presentes, exclui o script
+        Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+    }
+    else {
+        # Se alguma string estiver faltando, adiciona as strings em uma linha livre
+        Add-Content -Path $arquivoConfig -Value "`ng_language = portuguese_(brazil)`ng_languageAudio = english"
+
+        # Exclui o script
+        Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+    }
+}
+else {
+    # Se o arquivo não existir, cria o arquivo com as strings
+    Set-Content -Path $arquivoConfig -Value "g_language = portuguese_(brazil)`ng_languageAudio = english"
+
+    # Exclui o script
+    Remove-Item -Path $MyInvocation.MyCommand.Path -Force
+}
